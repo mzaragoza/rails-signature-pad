5710b2550d5a5531fc94b7d4dc5fe2daa0d40c53
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-07-22 17:07:02 -0400

    first commit

---
+++ lib/rails/signature/pad.rb
@@ -0,0 +1,9 @@
+require "rails/signature/pad/version"
+
+module Rails
+  module Signature
+    module Pad
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/signature/pad/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Signature
+    module Pad
+      VERSION = "0.0.1"
+    end
+  end
+end
